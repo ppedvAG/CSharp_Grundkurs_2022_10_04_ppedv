28a3fde29b6ad1c2188f9e2fa3ff2c18e3a8b11a
ppedvLukasK <lukask@ppedv.de>
2022-10-04 09:14:50 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharp_Grundkurs_2022_10_04
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CSharp_Grundkurs_2022_10_04
+ C# Grundkurs 04.10.2022
