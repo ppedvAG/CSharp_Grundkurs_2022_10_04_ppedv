6ffa01b18d9c7d10d985c45f8956bba1234e6cee
ppedvLukasK <lukask@ppedv.de>
2022-10-05 12:32:48 +0200

    M007

---
+++ M007/Fenster.cs
@@ -0,0 +1,61 @@
+﻿namespace M007;
+
+internal class Fenster
+{ 
+	#region Variable + Get/Set
+	private double laenge; //Felder sollten von aussen nicht angreifbar sein (nur über Methoden)
+	#endregion
+
+	#region Properties
+	private double breite;
+
+	private FensterStatus status;
+
+	public FensterStatus Status
+	{
+		get { return status; }
+		set { status = value; } //private set: nicht von außen setzbar (nur innerhalb der Klasse)
+	}
+
+	public void FensterOeffnen()
+	{
+		if (Status != FensterStatus.Offen)
+			Status = FensterStatus.Offen;
+		else
+			Console.WriteLine("Fenster ist bereits geöffnet");
+		//ZaehleFenster(); funktioniert
+	}
+	#endregion
+
+	public Fenster() //Wie Methode nur ohne Rückgabewert
+	{ }
+
+	public Fenster(double laenge, double breite)
+	{
+		this.laenge = laenge; //Mit this nach oben greifen auf die Variable Länge
+		this.breite = breite;
+		//ZaehleFenster();
+	}
+
+	~Fenster() //~ + Tab + Tab
+	{
+		Console.WriteLine($"Fenster wurde eingesammelt {laenge}x{breite}"); //Wird aufgerufen wenn der Garbage Collector das Objekt einsammelt
+	}
+
+	#region Static
+	public static int Zaehler = 0;
+
+	public static void ZaehleFenster()
+	{
+		Zaehler++;
+		//FensterOeffnen(); nicht möglich
+	}
+	#endregion
+}
+
+enum FensterStatus
+{
+	Geschlossen,
+	Gekippt,
+	Offen
+}
---
+++ M007/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace M007
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			#region GC
+			for (int i = 0; i < 25; i++)
+			{
+				Fenster f = new Fenster(i * 2, i * 5);
+			}
+
+			GC.Collect(); //Hier GC erzwingen
+			GC.WaitForPendingFinalizers(); //Warte auf alle Destruktoren
+			#endregion
+
+			#region Static
+			//Statische Member müssen ohne Objekt arbeiten
+			//Program p = new Program();
+			//p.Main(null); Statische Methoden können nicht über das Objekt sondern nur über den Klassennamen aufgerufen werden
+			//Program.Main(args);
+
+			Fenster.ZaehleFenster();
+			Console.WriteLine(Fenster.Zaehler); //Zaehler ist nicht auf Objekten sondern auf der Klasse selbst
+			#endregion
+
+			#region Werte/Referenztyp
+			//Wertetyp
+			int original = 5;
+			int x = original; //Kopie von 5
+			original = 10;
+
+			//Referenztyp
+			Fenster f1 = new Fenster();
+			Fenster f2 = f1; //Referenz zwischen f1 und f2
+			f1.Status = FensterStatus.Offen;
+
+			//class: Referenztyp, struct: Wertetyp
+			#endregion
+
+			#region Null
+			Fenster f3; //Standardwert: null
+			f3 = null;
+
+			//f3.FensterOeffnen(); //Nicht vorhandenes Fenster kann nicht geöffnet werden
+			if (f3 == null) //Wenn mein Fenster nicht existiert, erstelle ich eines
+			{
+				f3 = new Fenster();
+			}
+
+			if (f3 != null) //Vorher schauen ob mein Fenster existiert
+			{
+				f3.FensterOeffnen();
+			}
+			#endregion
+		}
+	}
+}
